5577cdff674e2bf40bc603dcfbfcde24e6180148
abonello <ab@anthonybonello.co.uk>
2019-01-16 14:07:05 +0000

    Clean unit testing file.

--- test_run.py
+++ test_run.py
@@ -52,12 +52,6 @@ class BasicTests(unittest.TestCase):
 
 
 
-        '''HOW TO DO THIS? IT REQUIRES AN ID
-        response = tester.get('/insert_food_item', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)'''
-
-
-
     def test_get_routes_no_params_connect_to_correct_template(self):
         tester = app.test_client(self)
 
@@ -84,36 +78,6 @@ class BasicTests(unittest.TestCase):
 
         response = tester.get('/contact', content_type="html/text")
         self.assertTrue(b'Contact Us' in response.data)
-
-
-    ''' 
-    HOW DO I DO SOMETHING LIKE THIS?
-    DO I NEED TO DO IT FOR THIS PROJECT?
-
-    def test_post_routes_no_params(self):
-        tester = app.test_client(self)
-
-        response = tester.post('/insert_food_item', 
-                    data=dict(
-                            classification="test", 
-                            name="test1", 
-                            brand="testBrand",
-                            energy1="1",
-                            energy2="1",
-                            fat="1",
-                            saturated="1",
-                            carbohydrates="1",
-                            sugar="1",
-                            fibre="1",
-                            protein="1",
-                            salt="1",
-                            shop="1",
-                            notes="1",
-                            action=""
-                            ), 
-                    follow_redirects=True)
-        self.assertIn(b'Food Items', response.data)'''
-
 
 if __name__ == "__main__":
     unittest.main()
